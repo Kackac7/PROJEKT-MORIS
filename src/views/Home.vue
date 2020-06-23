93b9369b837a03b872cdfad995188a552dd5967c
AdeKriz <AdeKriz@users.noreply.github.com>
2020-06-23 10:00:38 +0200

    seznamy, prihlasovani

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,10 +12,32 @@
       <v-row no-gutters class="my-15 mx-10">
 
         <v-col cols="3"> 
-          <v-btn class="main-page-button white--text" color="#302F2F">Registrace</v-btn>
+          <v-btn class="main-page-button white--text" color="#302F2F">Registrovat</v-btn>
         </v-col>
         <v-col cols="3">
-         <v-btn class="main-page-button white--text" color="#302F2F" v-on:click="login">Přihlásit</v-btn>
+        
+        <v-dialog max-width="214px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn class="main-page-button white--text" color="#302F2F" v-bind="attrs" v-on="on">Přihlásit</v-btn>
+          </template>
+            <v-form ref="form" class="white pa-5">
+              <span class="red--text" v-if="validationError">Chybné údaje</span>
+              <v-text-field
+                v-model="username"
+                label="Přihlašovací jméno"
+                required
+                v-on:keyup.enter="login"
+              ></v-text-field>
+              <v-text-field
+                :type="'password'"
+                v-model="password"
+                label="Heslo"
+                required
+                v-on:keyup.enter="login"
+              ></v-text-field>
+              <v-btn color="#302F2F" class="mr-4" v-on:click="login">Přihlásit</v-btn>
+            </v-form>
+        </v-dialog>
         </v-col>
 
       </v-row>
@@ -45,7 +67,69 @@ export default {
 
   data() {
     return {
-      userLoggedIn: false
+      userLoggedIn: false,
+      user: null,
+
+      dialog: false,
+
+      username: "",
+      password: "",
+
+      validationError: false,
+    }
+  },
+   methods: {
+    logout() {
+      userStore.store().user = null;
+      Bus.$emit("userLoggedOut");
+
+      this.userLoggedIn = false;
+      this.user = null;
+
+      this.menu = false;
+
+      Bus.$emit("showSnackbar", {
+        text: "Odhlášení úspěšné",
+        timeout: 3000
+      });
+    },
+
+    login() {
+      const getUsers = async () => {
+        const response = await fetch(
+          "https://crudcrud.com/api/e262c0cbc45743039a2870e26c04d0fe/users"
+        );
+        const json = await response.json();
+        if (json.length > 0) {
+          if (json[0] !== undefined) {
+            return json[0].users;
+          }
+        }
+      };
+
+      getUsers().then(users => {
+        this.validateUser(users);
+      });
+    },
+
+    validateUser(users) {
+      let filteredUsers = users.filter(user => {
+        return (
+          user.username === this.username && user.password === this.password
+        );
+      });
+      if (filteredUsers.length === 1) {
+        userStore.store().user = filteredUsers[0];
+        Bus.$emit("userLoggedIn");
+
+        this.user = filteredUsers[0];
+        this.userLoggedIn = true;
+
+        this.validationError = false;
+        this.menu = false;
+      } else {
+        this.validationError = true;
+      }
     }
   },
 
